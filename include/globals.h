167e2e3a408bc68fe9c71af6ed1bdef0486300d6
Geoffrey ARGENCE <geargenc@e1r3p9.42.fr>
2021-12-14 05:29:21 +0100

    [OPT] - added const keyword on a readonly global variable

--- include/globals.h
+++ include/globals.h
@@ -6,7 +6,7 @@
 /*   By: geargenc <geargenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 02:26:47 by geargenc          #+#    #+#             */
-/*   Updated: 2020/09/23 04:14:38 by geargenc         ###   ########.fr       */
+/*   Updated: 2021/12/14 04:48:47 by geargenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 t_root				g_root;
 
-t_zone_desc			g_zone_desctab[] =
+const t_zone_desc	g_zone_desctab[] =
 {
 	{"TINY", TINY_SIZE, TINY_ZONE_SIZE, TINY_STEP, &(g_root.tiny), &ft_find_space_zone, ft_realloc_notlarge},
 	{"SMALL", SMALL_SIZE, SMALL_ZONE_SIZE, SMALL_STEP, &(g_root.small), &ft_find_space_zone, ft_realloc_notlarge},
