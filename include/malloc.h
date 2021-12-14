a6ba10a957ac33efa2ababcb868da6c193efb4ff
Geoffrey ARGENCE <geargenc@e1r3p9.42.fr>
2021-12-14 05:47:43 +0100

    [FIX] - resolved confliting types for global variable adding  keyword to extern declaration

--- include/malloc.h
+++ include/malloc.h
@@ -6,7 +6,7 @@
 /*   By: geargenc <geargenc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 02:22:51 by geargenc          #+#    #+#             */
-/*   Updated: 2020/10/07 18:59:50 by geargenc         ###   ########.fr       */
+/*   Updated: 2021/12/14 05:42:45 by geargenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,106 +31,106 @@
 # include <stdio.h>
 # include <unistd.h>
 
-typedef struct			s_alloc
+typedef struct				s_alloc
 {
-	void				*addr_begin;
-	size_t				size;
-	struct s_alloc		*previous;
-	struct s_alloc		*next;
-}						t_alloc;
+	void					*addr_begin;
+	size_t					size;
+	struct s_alloc			*previous;
+	struct s_alloc			*next;
+}							t_alloc;
 
-typedef struct			s_zone
+typedef struct				s_zone
 {
-	void				*addr_begin;
-	struct s_alloc		*first;
-	struct s_zone		*previous;
-	struct s_zone		*next;
-}						t_zone;
+	void					*addr_begin;
+	struct s_alloc			*first;
+	struct s_zone			*previous;
+	struct s_zone			*next;
+}							t_zone;
 
-typedef struct			s_org
+typedef struct				s_org
 {
-	struct s_org		*previous;
-	struct s_org		*next;
-}						t_org;
+	struct s_org			*previous;
+	struct s_org			*next;
+}							t_org;
 
-typedef struct			s_root
+typedef struct				s_root
 {
-	struct s_zone		*tiny;
-	struct s_zone		*small;
-	struct s_zone		*large;
-	struct s_org		*org;
-	size_t				page_size;
-	size_t				org_link_size;
-	unsigned int		org_link_nb;
-	size_t				org_availability_size;
-}						t_root;
+	struct s_zone			*tiny;
+	struct s_zone			*small;
+	struct s_zone			*large;
+	struct s_org			*org;
+	size_t					page_size;
+	size_t					org_link_size;
+	unsigned int			org_link_nb;
+	size_t					org_availability_size;
+}							t_root;
 
-typedef struct			s_zone_desc
+typedef struct				s_zone_desc
 {
-	const char * const	name;
-	size_t				alloc_max_size;
-	size_t				zone_size;
-	size_t				step_size;
-	struct s_zone		**zone_ptr;
-	void				*(*f_allocate)(
-							size_t,
-							struct s_zone_desc *);
-	void				*(*f_realloc)(
-							t_alloc *,
-							t_zone *,
-							struct s_zone_desc *,
-							size_t);
-}						t_zone_desc;
+	const char * const		name;
+	size_t					alloc_max_size;
+	size_t					zone_size;
+	size_t					step_size;
+	struct s_zone			**zone_ptr;
+	void					*(*f_allocate)(
+								size_t,
+								struct s_zone_desc *);
+	void					*(*f_realloc)(
+								t_alloc *,
+								t_zone *,
+								struct s_zone_desc *,
+								size_t);
+}							t_zone_desc;
 
-extern t_root			g_root;
-extern t_zone_desc		g_zone_desctab[];
+extern t_root				g_root;
+extern const t_zone_desc	g_zone_desctab[];
 
-t_zone_desc				*ft_zone_desctab(
-							size_t size);
+t_zone_desc					*ft_zone_desctab(
+								size_t size);
 
-void					*ft_allocate_zone(
-							size_t size,
-							t_zone_desc *zone_desc);
-void					*ft_find_space_zone(
-							size_t size,
-							t_zone_desc *zone_desc);
-void					*ft_malloc(
-							size_t size);
+void						*ft_allocate_zone(
+								size_t size,
+								t_zone_desc *zone_desc);
+void						*ft_find_space_zone(
+								size_t size,
+								t_zone_desc *zone_desc);
+void						*ft_malloc(
+								size_t size);
 
-void					ft_free_alloc(
-							t_alloc *alloc,
-							t_zone *zone,
-							t_zone_desc *zone_desc);
-void					ft_free(
-							void *addr);
+void						ft_free_alloc(
+								t_alloc *alloc,
+								t_zone *zone,
+								t_zone_desc *zone_desc);
+void						ft_free(
+								void *addr);
 
-void					ft_org_free(
-							void *addr);
-void					*ft_org_alloc(void);
-void					ft_org_init(void) __attribute__((constructor));
+void						ft_org_free(
+								void *addr);
+void						*ft_org_alloc(void);
+void						ft_org_init(void) __attribute__((constructor));
 
-void					*ft_realloc_large(
-							t_alloc *alloc,
-							t_zone *zone,
-							t_zone_desc *zone_desc,
-							size_t size);
-void					*ft_realloc_notlarge(
-							t_alloc *alloc,
-							t_zone *zone,
-							t_zone_desc *zone_desc,
-							size_t size);
-void					*ft_realloc(
-							void *addr,
-							size_t size);
+void						*ft_realloc_large(
+								t_alloc *alloc,
+								t_zone *zone,
+								t_zone_desc *zone_desc,
+								size_t size);
+void						*ft_realloc_notlarge(
+								t_alloc *alloc,
+								t_zone *zone,
+								t_zone_desc *zone_desc,
+								size_t size);
+void						*ft_realloc(
+								void *addr,
+								size_t size);
 
-t_alloc					*ft_find_alloc(
-							void *addr,
-							t_zone *zone);
-t_zone					*ft_find_zone(
-							void *addr,
-							t_zone_desc *zone_desc);
+t_alloc						*ft_find_alloc(
+								void *addr,
+								t_zone *zone);
+t_zone						*ft_find_zone(
+								void *addr,
+								t_zone_desc *zone_desc);
 
-void					ft_show_alloc_mem(void);
+void						ft_show_alloc_mem(void);
 // t_alloc			*lookforspace(void *current, t_alloc *next, size_t current_len, size_t len)
 // {
 // 	if (next - current - current_len < len)
