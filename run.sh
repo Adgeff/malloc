3e35ef934b8fdfa2dc4b5a9b6637cba58c6bf301
Geoffrey ARGENCE <geargenc@e2r6p9.clusters.42paris.fr>
2022-03-15 16:37:41 +0100

    [REFACT] - Tabs were replaced with spaces

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ $(uname -s) = "Linux" ]
 then
-    export LD_PRELOAD=./libft_malloc.so
+	export LD_PRELOAD=./libft_malloc.so
 elif [ $(uname -s) = "Darwin" ]
 then
-    export DYLD_INSERT_LIBRARIES=./libft_malloc.so
-    export DYLD_FORCE_FLAT_NAMESPACE=1
+	export DYLD_INSERT_LIBRARIES=./libft_malloc.so
+	export DYLD_FORCE_FLAT_NAMESPACE=1
 fi
 exec $@
